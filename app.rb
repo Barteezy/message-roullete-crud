b25549915e2ce0dba3f1c0497492a7f6bea43c2c
Luke Bartel <bartelluke@gmail.com>
2014-07-31 13:26:38 -0600

    user can add comments

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require "sinatra"
 require "active_record"
 require "gschool_database_connection"
 require "rack-flash"
+require "./lib/messages_table"
+require "./lib/comment_table"
 
 class App < Sinatra::Application
   enable :sessions
@@ -10,22 +12,68 @@ class App < Sinatra::Application
   def initialize
     super
     @database_connection = GschoolDatabaseConnection::DatabaseConnection.establish(ENV["RACK_ENV"])
+    @message_table = MessagesTable.new(GschoolDatabaseConnection::DatabaseConnection.establish(ENV["RACK_ENV"]))
+    @comment_table = CommentsTable.new(GschoolDatabaseConnection::DatabaseConnection.establish(ENV["RACK_ENV"]))
   end
 
   get "/" do
-    messages = @database_connection.sql("SELECT * FROM messages")
+    messages = @message_table.all
+    comments = @comment_table.all
+    erb :home, locals: {messages: messages,
+                        comments: comments
 
-    erb :home, locals: {messages: messages}
+    }
   end
 
   post "/messages" do
     message = params[:message]
     if message.length <= 140
-      @database_connection.sql("INSERT INTO messages (message) VALUES ('#{message}')")
+      @message_table.create(message)
     else
       flash[:error] = "Message must be less than 140 characters."
     end
     redirect "/"
   end
+
+  get "/edit/:id" do
+    message = @message_table.find(params[:id])
+    p message["id"]
+    erb :edit_message, locals: {message: message}
+  end
+
+  patch "/edit/:id" do
+    @message_table.update(params[:message], params[:id])
+    redirect "/"
+  end
+
+  delete "/delete/:id" do
+    @message_table.delete(params[:id])
+    redirect "/"
+  end
+
+  get "/comment/:id" do
+    message = @message_table.find(params[:id])
+    erb :add_comment, locals: {message: message}
+  end
+
+  post "/comment/:id" do
+    @comment_table.create(params[:comment], params[:id])
+    redirect "/"
+  end
+
+  post "/like/:id" do
+    like = @database_connection.sql("SELECT like_num from messages where id = #{params[:id]}").first
+
+    if like["like_num"]
+      likes = like["like_num"].to_i
+    else
+      likes = 0
+    end
+
+    likes += 1
+    @database_connection.sql("UPDATE messages set like_num = #{likes} where id = #{params[:id]}").first
+    redirect "/"
+  end
+
 
 end
--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -23,4 +23,64 @@ feature "Messages" do
 
     expect(page).to have_content("Message must be less than 140 characters.")
   end
+
+  scenario "As a user, I can edit a message" do
+    visit "/"
+
+    expect(page).to have_content("Message Roullete")
+
+    fill_in "Message", :with => "Hello Everyone!"
+
+    click_button "Submit"
+
+    expect(page).to have_content("Hello Everyone!")
+
+    click_on "edit"
+
+    expect(page).to have_content("Edit Message")
+
+    fill_in "message", :with => "I said, Hello Everyone!"
+
+    click_on "Edit"
+
+    expect(page).to have_content("I said, Hello Everyone!")
+
+    click_on "Delete"
+
+    expect(page).to_not have_content("I said, Hello Everyone!")
+  end
+
+  scenario "As a user I can add a comment to a message" do
+      visit "/"
+
+      expect(page).to have_content("Message Roullete")
+
+      fill_in "Message", :with => "Hello Everyone!"
+
+      click_button "Submit"
+
+      expect(page).to have_content("Hello Everyone!")
+
+      click_on "edit"
+
+      expect(page).to have_content("Edit Message")
+
+      fill_in "message", :with => "I said, Hello Everyone!"
+
+      click_on "Edit"
+
+      expect(page).to have_content("I said, Hello Everyone!")
+
+      click_on "Comment"
+
+      expect(page).to have_content("Add a comment")
+
+      fill_in "comment", :with => "Here is a comment"
+
+      click_on "Add Comment"
+
+      expect(page).to have_content "Here is a comment"
+
+
+  end
 end
